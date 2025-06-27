77fb8cca70f1caf32a89daa0e6a36032f9d9c369
Sertavr <serg.tadadz@gmail.com>
2025-06-27 18:12:49 +0300

    Added new Welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -121,6 +121,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -129,6 +133,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResult);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Hello');
+  newFeature();
 };
 init();
