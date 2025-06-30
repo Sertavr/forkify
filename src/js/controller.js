01ad2b7ceacab5d066bb1493cacac089f14de17b
Sertavr <serg.tadadz@gmail.com>
2025-06-30 08:52:18 +0300

    deleted duplicate import

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,7 +2,6 @@ import * as model from './model.js';
 import { MODAL_CLOSE_SEC } from './config.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
-import recipeView from './views/recipeView.js';
 import bookmarksView from './views/bookmarksView.js';
 import paginationView from './views/paginationView.js';
 import addRecipeView from './views/addRecipeView.js';
