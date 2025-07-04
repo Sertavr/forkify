473e9d5e5812aaf060ee83dade14abde67c00980
Sertavr <serg.tadadz@gmail.com>
2025-07-04 13:23:18 +0300

    import

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,8 +6,7 @@ import bookmarksView from './views/bookmarksView.js';
 import paginationView from './views/paginationView.js';
 import addRecipeView from './views/addRecipeView.js';
 
-import 'core-js/actual';
-//import 'core-js/stable';
+import { core} from 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
 import resultsView from './views/resultsView.js';
